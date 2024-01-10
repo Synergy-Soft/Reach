bdcbed736a25c63b27bad5e0cee21b69ca8fd186
RJ Zanotti <rj.zanotti19@gmail.com>
2024-01-10 18:31:28 -0500

    Homepage

--- reach-web/src/pages/Home.js
+++ reach-web/src/pages/Home.js
@@ -1,22 +1,72 @@
 import React, { useState} from "react"
-import { Grid, Box, Typography } from "@mui/material"
+import { Grid, Box, Typography, Button } from "@mui/material"
 
-const Home = () => { 
+const Home = () => {
     // logic
-
-
 
     // jsx
     return (
-        <>
-            <Grid container>
-                <Grid item sx={{  }} xs={12}>
-                    <Typography>Home Page</Typography>
-
-                </Grid>
+        <Grid container>
+            <Grid item xs={12}>
+                <Box
+                    sx={{
+                        flexShrink: 0,
+                        backgroundColor: '#032F73',
+                        position: 'relative',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'flex-end',
+                        alignItems: 'flex-start',
+                    }}
+                >
+                    <Typography
+                        variant="body1"
+                        sx={{
+                            color: 'white',
+                            fontFamily: 'Inter',
+                            fontStyle: 'normal',
+                            fontSize: '3vw',
+                            fontWeight: 'bold',
+                            m: 4,
+                            t: 1,
+                        }}
+                    >
+                        Your Gateway to Amplified Brand Exposure
+                    </Typography>
+                    <Button
+                        variant="contained"
+                        sx={{
+                            position: 'relative',
+                            top: '-20px',
+                            right: '-20px',
+                        }}
+                    >
+                        Learn More
+                    </Button>
+                </Box>
             </Grid>
-        </>
+            <Grid container>
+                <Box 
+                    sx={{
+                        backgroundColor: '#FFF1EE',
+                        marginLeft: 'auto',
+                        marginRight: '32px',
+                        l: '16px', 
+                        r: '16px', 
+                        height: '40vh',
+                        width: '60vh',                      
+                    }}
+                >
+                    <Typography sx  ={{
+                        color: '#FF7F63',
+                        fontFamily: 'Poppins',
+                        fontStyle: 'normal',
+                        fontWeight: 'Bold',
+                    }}>Reach</Typography>
+                </Box>
+            </Grid>
+        </Grid>
     );
-}
+};
 
 export default Home;
