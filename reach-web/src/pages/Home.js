4fa736cb17c2d1f2db21526a4943abc6d8f45f5e
RyanMalloy <129435196+RyanMalloy@users.noreply.github.com>
2024-02-19 20:00:05 -0500

    firebase setup

--- reach-web/src/pages/Home.js
+++ reach-web/src/pages/Home.js
@@ -1,27 +1,17 @@
-import React, { useState } from "react";
-import { Grid, Box, Typography, Button, Card, CardHeader, CardContent, CardActionArea } from "@mui/material";
-import CampaignCard from "../components/Cards/CampaignCard";
-import { YouTube } from "@mui/icons-material";
+import { Grid } from "@mui/material";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   // logic
 
   // jsx
   return (
+    <>
+    <Navbar />
     <Grid container>
-      <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-around", gap: 5 }}>
-        <Box>
-          <Typography variant="h3">
-            Your Gateway to<br></br> Amplified Brand Exposure
-          </Typography>
-          <Button variant="contained">Learn More</Button>
-        </Box>
-
-        <Box>
-          <CampaignCard icon={<YouTube/>} />
-        </Box>
-      </Box>
+      <h1>Home Page</h1>
     </Grid>
+    </>
   );
 };
 
